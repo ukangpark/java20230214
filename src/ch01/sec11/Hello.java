a8316d8db8049977aeace8c1a743334f6b1670f8
ukangpark <ukang0711@naver.com>
2023-02-14 10:54:34 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,19 @@
+package ch01.sec11;
+	/**
+	 * @author 여러분의 이름
+	 */
+	/*
+	장제목: 1장 자바 시작하기
+	작성일: 2023.02.14
+	 */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 한줄 주석
+		/*
+		 * 여러줄 주석
+		 */
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,10 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		System.out.println("Hi JAVA");
+		System.out.println("출력 되는 내용");
+		System.out.println("출력!!");
+	}
+
+}
